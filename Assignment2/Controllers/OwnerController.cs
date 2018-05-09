1c18c6083f27972dd63bfa30b8f156d450f22267
psuresan12 <psuresan12@gmail.com>
2018-05-09 20:09:49 +1000

    add owner inventory

--- Assignment2/Controllers/OwnerController.cs
+++ Assignment2/Controllers/OwnerController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Assignment2.Data;
+using Assignment2.Models;
+using Assignment2.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Assignment2.Controllers
 {
-    public class OwnerController:Controller
+    public class OwnerController : Controller
     {
         private readonly ApplicationDbContext _context;
 
@@ -17,28 +19,65 @@ namespace Assignment2.Controllers
             _context = context;
         }
 
+
         // Auto-parsed variables coming in from the request - there is a form on the page to send this data.
-        public async Task<IActionResult> Index(string productName)
+        public async Task<IActionResult> Index(
+            string sortOrder, string currentFilter,
+            string searchString, int? page)
         {
-            
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["QtySortParm"] = sortOrder == "Qty" ? "qty_desc" : "Qty";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewData["CurrentFilter"] = searchString;
+
+
+
             // Eager loading the Product table - join between OwnerInventory and the Product table.
             var query = _context.OwnerInventory.Include(x => x.Product).Select(x => x);
 
-            if (!string.IsNullOrWhiteSpace(productName))
+            if (!string.IsNullOrWhiteSpace(searchString))
             {
-                // Adding a where to the query to filter the data.
-                // Note for the first request productName is null thus the where is not always added.
-                query = query.Where(x => x.Product.Name.Contains(productName));
+                query = query.Where(x => x.Product.Name.Contains(searchString));
 
-                // Storing the search into ViewBag to populate the textbox with the same value for convenience.
-                ViewBag.ProductName = productName;
             }
 
-            // Adding an order by to the query for the Product name.
-            //// query = query.OrderBy(x => x.ProductID);
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    query = query.OrderByDescending(s => s.Product.Name);
+                    break;
+                case "Qty":
+                    query = query.OrderBy(s => s.StockLevel);
+                    break;
+                case "date_desc":
+                    query = query.OrderByDescending(s => s.StockLevel);
+                    break;
+                default:
+                    query = query.OrderBy(s => s.Product.Name);
+                    break;
+            }
 
-            // Passing a List<OwnerInventory> model object to the View.
-            return View(await query.ToListAsync());
+            int pageSize = 3;
+            //var viewModel = new OwnerInventoryViewModel
+            //{
+            //    Inventory = await PaginatedList<OwnerInventory>
+            //       .CreateAsync(query.AsNoTracking(), page ?? 1, pageSize)
+            //};
+
+            //// Passing a List<OwnerInventory> model object to the View.
+            //return View(viewModel);
+
+            return View(await PaginatedList<OwnerInventory>
+                        .CreateAsync(query.AsNoTracking(), page ?? 1, pageSize));
         }
 
 
@@ -47,6 +86,5 @@ namespace Assignment2.Controllers
             return "this is a owner page";
         }
     }
-
 
 }
--- Assignment2/Data/ApplicationDbContext.cs
+++ Assignment2/Data/ApplicationDbContext.cs
@@ -11,8 +11,10 @@ namespace Assignment2.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
       
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Store> Store { get; set; }
         public DbSet<OwnerInventory> OwnerInventory { get; set; }
-
+        public DbSet<StockRequest> StockRequest { get; set; }
 
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
