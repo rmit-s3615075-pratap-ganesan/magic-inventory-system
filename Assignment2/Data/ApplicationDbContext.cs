8587a4dd4fd3505e9f89f749777c50f72a2b576c
Niraj Bohra <s3578115@student.rmit.edu.au>
2018-05-10 20:00:05 +1000

    Post migration Content

--- Assignment2/Data/ApplicationDbContext.cs
+++ Assignment2/Data/ApplicationDbContext.cs
@@ -11,11 +11,12 @@ namespace Assignment2.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
       
-        public DbSet<Product> Product { get; set; }
-        public DbSet<Store> Store { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
         public DbSet<OwnerInventory> OwnerInventory { get; set; }
-        public DbSet<StockRequest> StockRequest { get; set; }
         public DbSet<StoreInventory> StoreInventory { get; set; }
+        public DbSet<StockRequest> StockRequest { get; set; }
+
 
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -27,6 +28,9 @@ namespace Assignment2.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<StoreInventory>().HasKey(x => new { x.StoreID, x.ProductID });
+       
+            
+           
         }
     }
 }
---
+++ Assignment2/Migrations/20180510055057_UserStoreID.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Assignment2.Migrations
+{
+    public partial class UserStoreID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StoreID",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_StoreID",
+                table: "AspNetUsers",
+                column: "StoreID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Stores_StoreID",
+                table: "AspNetUsers",
+                column: "StoreID",
+                principalTable: "Stores",
+                principalColumn: "StoreID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Stores_StoreID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_StoreID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StoreID",
+                table: "AspNetUsers");
+        }
+    }
+}
