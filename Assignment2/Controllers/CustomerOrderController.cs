db2caa4454d997fca82e76a5cd9c2621ed3ebc92
psuresan12 <psuresan12@gmail.com>
2018-05-18 20:19:03 +1000

    showing only the logged in orders

--- Assignment2/Controllers/CustomerOrderController.cs
+++ Assignment2/Controllers/CustomerOrderController.cs
@@ -7,19 +7,34 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using Assignment2.Models;
 using Assignment2.Models.OrderHistoryModels;
+using Assignment2.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace Assignment2.Controllers
 {
     public class CustomerOrderController : Controller
     {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public CustomerOrderController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         // GET: /<controller>/
         public async Task<IActionResult> Index(int? id)
         {
+            var user = await _userManager.GetUserAsync(User);
+
+
             var viewModel = new HistoryGroupData();
             using (var client = new HttpClient())
             {
                 var result = await client.GetStringAsync("http://localhost:5000/api/orders");
                 viewModel.customerOrders = JsonConvert.DeserializeObject<List<CustomerOrder>>(result);
+                viewModel.customerOrders = viewModel.customerOrders.Select(x => x).Where(x => x.UserEmail == user.Email).Reverse();
 
             }
 
@@ -37,4 +52,3 @@ namespace Assignment2.Controllers
         }
     }
 }
-
